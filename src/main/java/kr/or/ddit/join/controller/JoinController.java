1355eb6bec67a0d79b0d5a031353976e1d13bbf8
jyh6842 <jyh6842@gmail.com>
2020-07-22 10:15:30 +0900

    log4j => logback(AOP)

--- src/main/java/kr/or/ddit/join/controller/JoinController.java
+++ src/main/java/kr/or/ddit/join/controller/JoinController.java
@@ -28,6 +28,9 @@ import org.springframework.web.servlet.support.RequestContextUtils;
 @Controller /*클라이언트 요청을 처리할 수  있는 클래스가 됨*/
 @RequestMapping("/user/join/") /*폼 채크 아웃이 공통으로 쓰는거는 이렇게 미리 선언할 수 있음*/
 public class JoinController {
+//	private Logger loger = LoggerFactory.getLogger(this.getClass()); logback를 사용하지 않으면 각각의 클래스 마다 이걸 선언해 줘야하고
+	// 로그를 찍을 시점(메소드) 안에 로그를 찍게 만들어줘야 한다.
+	// 정상적으로 시작 되었는지 정상적으로 끝났는지 익셉션 됬는지 일일이 전부 로그를 만들어 줘야한다. AOP를 사용하면 알아서 해준다.
 	@Autowired
 	private MessageSourceAccessor accessor; // 다국어 지원 모드를 위해서
 	
@@ -37,7 +40,7 @@ public class JoinController {
 	@Autowired
 	private CryptoGenerator cryptoGenerator;
 	
-	   @RequestMapping(value="loginCheck", method=RequestMethod.POST, params={"mem_id=a001"})
+	   @RequestMapping(value="loginCheck", method=RequestMethod.GET, params={"mem_id=a001"})
 	   public String loginCheckHeader(String mem_id, String mem_pass, 
 	         HttpServletRequest request, 
 	         HttpSession session, 
